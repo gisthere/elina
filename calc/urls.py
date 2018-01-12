f6fe29f2f40ced178eef6395d2d187311c6c7c4c
Fanis Kalimullin <fmkalimullin@gmail.com>
2018-01-12 15:07:30 +0300

    Added page for Shewhart maps

--- calc/urls.py
+++ calc/urls.py
@@ -4,4 +4,5 @@ from calc import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^shewhart$', views.ShewhartView.as_view(), name='shewhart'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -38,3 +38,38 @@ class IndexView(TemplateView):
                                          delta_l_v=delta_l_v))
 
         return response
+
+
+class ShewhartView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        response = TemplateResponse(request, "shewhart.html")
+        return response
+
+    def post(self, request, *args, **kwargs):
+        x1 = [value for key, value in sorted(request.POST.items()) if
+              key.startswith("x1_")]
+        x1 = pd.Series(x1, name='x1', dtype='float32')
+        x2 = [value for key, value in sorted(request.POST.items()) if
+              key.startswith("x2_")]
+        x2 = pd.Series(x2, name='x2', dtype='float32')
+        data = pd.concat([x1, x2], axis=1)
+
+        r = request.POST['r']
+        R = request.POST['R']
+        mr = request.POST['mr']
+        n = int(request.POST['n'])
+        k = float(request.POST['k'])
+        delta_0 = float(request.POST['delta_0'])
+
+        sko = Sko(data, n, k, delta_0)
+        s = sko.get_s()
+        delta_s_l_v = sko.get_delta_s_l_v()
+        delta_l_v = sko.get_delta_l_v()
+        # mr,s,delta_s_l_v,delta_l_v
+
+        response = TemplateResponse(request, "result.html",
+                                    dict(mr=mr, s=s,
+                                         delta_s_l_v=delta_s_l_v,
+                                         delta_l_v=delta_l_v))
+
+        return response
